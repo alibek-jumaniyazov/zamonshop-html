70ce304348146e4cf6c8364472f0b4a875e927c5
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-05-05 18:11:36 +0500

    savat, forms, models add :ambulance:

--- backend/products/models.py
+++ backend/products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from mptt.models import MPTTModel
 from mptt.fields import TreeForeignKey
 
+from users.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -40,3 +42,54 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class BasketQuerySet(models.QuerySet):
+
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+# Savat 
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+
+    def __str__(self) -> str:
+        return f"{self.user.username} uchun Savat | Maxsulot: {self.product.name}"
+        
+    def sum(self):
+        return self.product.price * self.quantity
+
+
+    def de_json(self):
+        basket_item = {
+            'product_name': self.product.name,
+            'quantity': self.quantity,
+            'price': float(self.product.price),
+            'sum': float(self.sum()),
+        }
+        return basket_item
+
+    @classmethod
+    def create_or_update(cls, product_id, user):
+        baskets = Basket.objects.filter(user=user, product_id=product_id)
+
+        if not baskets.exists():
+            obj = Basket.objects.create(user=user, product_id=product_id, quantity=1)
+            is_created = True
+            return obj, is_created
+        else:
+            basket = baskets.first()
+            basket.quantity += 1
+            basket.save()
+            is_crated = False
+            return basket, is_crated
+
